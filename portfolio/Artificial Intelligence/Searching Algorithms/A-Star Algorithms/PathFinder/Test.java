4dab36cce3913db388e3de21ffd940ee9f0bb6aa
ubeydkarasahin <ubeyd.karasahin@gmail.com>
2020-04-03 15:02:21 -0400

    editing

--- portfolio/Artificial Intelligence/Searching Algorithms/A-Star Algorithms/PathFinder/Test.java
+++
@@ -1,6 +0,0 @@
-public class Test {
-    public static void main(String[] args){
-        AStar.test(1, 5, 5, 0, 0, 3, 2, new int[][]{{0,4},{2,2},{3,1},{3,3}});
-        AStar.test(2, 5, 5, 0, 0, 4, 4, new int[][]{{3,4},{3,3},{4,3}});
-    }
-}
--- portfolio/Basic Lua Interpreter/Test.java
+++
@@ -1,6 +0,0 @@
-public class Test {
-    public static void main(String[] args){
-        Parser parser = new Parser();
-        parser.program();
-    }
-}
